73583108803936b41e62587ee2d43ccd55cea2f8
Samuel Gaus <sam@gaus.co.uk>
2022-10-14 10:33:49 +0100

    Format web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,30 @@
-import { useEffect, useState } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Brush } from 'recharts';
+import { useEffect, useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  Brush,
+} from "recharts";
 import { roundToNearestMinutes, format } from "date-fns";
 
-type Price = { time: number, red: number, white: number, blue: number };
+type Price = { time: number; red: number; white: number; blue: number };
 
 function DateFormatter(time: number) {
   const date = new Date(time);
 
-  return format(roundToNearestMinutes(date, { nearestTo: 30, roundingMethod: "floor" }), "P HH:mm");
+  return format(
+    roundToNearestMinutes(date, { nearestTo: 30, roundingMethod: "floor" }),
+    "P HH:mm"
+  );
 }
 
 export default function App() {
   const [data, setData] = useState([] as Price[]);
-  
+
   useEffect(() => {
     async function load() {
       const response = await fetch("http://beefy-boy.local:3011/prices");
@@ -45,7 +57,12 @@ export default function App() {
         <Line type="monotone" dataKey="red" stroke="#ff0000" />
         <Line type="monotone" dataKey="white" stroke="#aaaaaa" />
         <Line type="monotone" dataKey="blue" stroke="#0000ff" />
-        <Brush dataKey="time" height={30} stroke="#8884d8" tickFormatter={DateFormatter} />
+        <Brush
+          dataKey="time"
+          height={30}
+          stroke="#8884d8"
+          tickFormatter={DateFormatter}
+        />
       </LineChart>
     </div>
   );
